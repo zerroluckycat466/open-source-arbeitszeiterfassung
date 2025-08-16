9e77a02a4d3d60963aa7f8c44dc76a51088868c3
zerroluckycat466 <m98jtlgb0nbs@hotmail.com>
2025-08-16 23:27:36 +0800

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Arbeitszeiterfassung</h1>
+      <p>Erfasse und verwalte deine Arbeitszeiten.</p>
+    </div>
+  );
+}
+
+export default App;
